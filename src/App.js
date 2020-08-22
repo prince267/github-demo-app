8f3af637a1a2e827568e3b5848916b603e0a2078
Prince Kumar <prince.kumar267@gmail.com>
2020-08-23 01:27:48 +0530

    Follow and get users

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends React.Component{
-constructor(){
-  
-}
-  componentDidMount(){
-
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import "./App.css";
+const { Octokit } = require("@octokit/core");
+const octokit = new Octokit({
+  auth: "27d9fa7ee7e825d1550a09e579fce8abb580690c",
+});
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      forkedUsers: [],
+      pageNumber: 1,
+    };
+    this.getForkedUsers = this.getForkedUsers.bind(this);
+    this.followUser = this.followUser.bind(this);
+  }
+  componentDidMount() {
+    this.getForkedUsers();
   }
 
-  getForkedUsers(){
-    
+  previousPage() {
+    this.setState({ pageNumber: this.state.pageNumber - 1 });
   }
-render(){
-  return(
-    <div>
-      Hello World
-    </div>
-  )
-}
 
+  nextPage() {
+    this.setState({ pageNumber: this.state.pageNumber + 1 });
+  }
+  async getForkedUsers() {
+    const forkedUsers = await octokit.request(
+      "GET /repos/{owner}/{repo}/forks",
+      {
+        owner: "facebook",
+        repo: "react",
+        per_page: 30,
+        page: this.state.pageNumber,
+      }
+    );
+    console.log("Forked users Length **** ", forkedUsers.data);
+    this.setState({ forkedUsers: forkedUsers.data });
+  }
+
+  async followUser(username) {
+    const response = await octokit.request("PUT /user/following/{username}", {
+      username: username,
+    });
+    console.log("Response ***** ", response);
+  }
+  render() {
+    return (
+      <div>
+        Hello World
+        <div className="wrapper">
+          {this.state.forkedUsers.map((data, index) => (
+            <div
+              style={{ display: "flex", justifyContent: "center" }}
+              key={index}
+            >
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src={data.owner.avatar_url} />
+                <Card.Body>
+                  <Card.Title>{data.owner.login}</Card.Title>
+                  <Card.Text>{data.description}</Card.Text>
+                  <Button
+                    variant="primary"
+                    onClick={() => {
+                      this.followUser(data.owner.login);
+                    }}
+                  >
+                    Follow
+                  </Button>
+                </Card.Body>
+              </Card>
+            </div>
+          ))}
+        </div>
+        <button
+          onClick={() => {
+            this.nextPage();
+            this.getForkedUsers();
+          }}
+        >
+          Next {this.state.pageNumber}
+        </button>
+        <button
+          onClick={() => {
+            this.previousPage();
+            this.getForkedUsers();
+          }}
+        >
+          Prev {this.state.pageNumber}
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
